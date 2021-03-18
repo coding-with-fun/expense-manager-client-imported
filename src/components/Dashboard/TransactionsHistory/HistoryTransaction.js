69b4a0488cfa4d9fe21c14bbab2ea70816a8d1c6
Coderc <dev@coderc.in>
2021-03-18 08:32:27 +0530

    Added Popover for action items

--- src/components/Dashboard/TransactionsHistory/HistoryTransaction.js
+++ src/components/Dashboard/TransactionsHistory/HistoryTransaction.js
@@ -1,17 +1,42 @@
-import { Paper } from '@material-ui/core';
+import { Paper, Popover } from '@material-ui/core';
 import ArrowRightIcon from '@material-ui/icons/ArrowRight';
-import React from 'react';
+import React, { useState } from 'react';
 
 const HistoryTransaction = () => {
+    const [anchorEl, setAnchorEl] = useState(null);
+    const open = Boolean(anchorEl);
+
+    const handleClick = (event) => {
+        setAnchorEl(event.currentTarget);
+    };
+
+    const handleClose = () => {
+        setAnchorEl(null);
+    };
+
     return (
         <Paper variant="outlined" className="history-transaction-container">
             <div className="details">
                 <div className="title">Hello</div>
                 <div className="amount">$300</div>
             </div>
-            <div className="success-status">
+            <div className="success-status" onClick={handleClick}>
                 <ArrowRightIcon />
             </div>
+            <Popover
+                open={open}
+                anchorEl={anchorEl}
+                onClose={handleClose}
+                anchorOrigin={{
+                    vertical: 'center',
+                    horizontal: 'right',
+                }}
+                transformOrigin={{
+                    vertical: 'center',
+                    horizontal: 'left',
+                }}>
+                The content of the Popover.
+            </Popover>
         </Paper>
     );
 };
