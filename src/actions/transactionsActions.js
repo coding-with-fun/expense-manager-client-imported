cb8e1ee645b2a9b124f60438a30a47a64e6aca48
Coderc <dev@coderc.in>
2021-03-24 21:38:15 +0530

    Removed unused actions and reducers

--- src/actions/transactionsActions.js
+++ src/actions/transactionsActions.js
@@ -1,42 +1,8 @@
-export const getAllTransactions = () => {
-    return {
-        type: 'GET_ALL_TRANSACTIONS',
-    };
-};
-
 export const setTransactions = (data) => {
     return {
         type: 'SET_TRANSACTIONS',
         payload: {
             data,
-        },
-    };
-};
-
-export const insertTransaction = (entry) => {
-    return {
-        type: 'INSERT_TRANSACTION',
-        payload: {
-            entry,
-        },
-    };
-};
-
-export const updateTransaction = (data, index) => {
-    return {
-        type: 'UPDATE_TRANSACTION',
-        payload: {
-            data,
-            index,
-        },
-    };
-};
-
-export const deleteTransaction = (index) => {
-    return {
-        type: 'DELETE_TRANSACTION',
-        payload: {
-            index,
         },
     };
 };
--- src/reducers/transactionsReducer.js
+++ src/reducers/transactionsReducer.js
@@ -4,45 +4,15 @@ const initialState = {
 
 const transactionsReducer = (state = initialState, action) => {
     switch (action.type) {
-        case 'GET_ALL_TRANSACTIONS':
-            return state;
-
         case 'SET_TRANSACTIONS':
             action.payload.data.sort(
                 (a, b) => parseFloat(b.date) - parseFloat(a.date)
             );
+
             return {
                 ...state,
                 transactions: [...action.payload.data],
             };
-
-        case 'INSERT_TRANSACTION': {
-            return {
-                ...state,
-                transactions: [...state.transactions, action.payload.entry],
-            };
-        }
-
-        case 'UPDATE_TRANSACTION': {
-            const updatedTransaction = action.payload.data;
-            let transactionList = [...state.transactions];
-            transactionList[action.payload.index] = updatedTransaction;
-
-            return {
-                ...state,
-                transactions: transactionList,
-            };
-        }
-
-        case 'DELETE_TRANSACTION': {
-            let transactionList = [...state.transactions];
-            transactionList.splice(action.payload.index, 1);
-
-            return {
-                ...state,
-                transactions: transactionList,
-            };
-        }
 
         default:
             return state;
