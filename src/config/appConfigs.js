a130f74af56f22fba8fd1cee9043a503fa124f0f
Coderc <dev@coderc.in>
2021-03-25 10:55:02 +0530

    Updated endpoints

--- src/config/appConfigs.js
+++ src/config/appConfigs.js
@@ -1,5 +1,5 @@
 const expenseManagerRoot = process.env.REACT_APP_EXPENSE_MANAGER_ROOT;
 
 export const endpoints = {
-    expenseManagerRoot: expenseManagerRoot + '/api',
+    expenseManagerRoot: expenseManagerRoot + '/api/expense-manager',
 };
